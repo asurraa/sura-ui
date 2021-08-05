e052ff410feb206120b8e02f9d0a0de035d298f2
lyhourchhen <ukamilove66@gmail.com>
2021-08-05 23:12:09 +0700

    update

--- packages/utilities/src/common/getIsProductionMode.ts
+++ packages/utilities/src/common/getIsProductionMode.ts
@@ -2,11 +2,7 @@
 // * Check Weather Developments is Production or developments.
 // **
 
-export const getIsProductionModeFunc = ({
-  log,
-}: {
-  log?: boolean;
-}): boolean => {
+export const getIsProductionModeFunc = ({}: { log?: boolean }): boolean => {
   if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
     return false;
   } else {
