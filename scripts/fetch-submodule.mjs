2feb002aee33795bcf0fa962637eae8d5d3be528
lyhourchhen <ukamilove66@gmail.com>
2021-08-12 15:15:16 +0700

    update

--- scripts/fetch-submodule.mjs
+++ scripts/fetch-submodule.mjs
@@ -3,7 +3,3 @@
 await $`echo "fetching submodule starting !!!"`;
 await $`git submodule init`;
 await $`git submodule update`;
-await $`cd asurraa-react-admin-starter`;
-await $`git checkout sura-ui-playground`;
-await $`cd ..`;
-await $`exiting script fetch submodule"`;
